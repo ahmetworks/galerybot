6ca0330321291148cf376ac31601d72c74354746
thegears <semh.1362@gmail.com>
2022-07-30 22:23:22 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+const { Client } = require("discord.js-selfbot-v13");
+const client = new Client({
+	checkUpdate: false,
+});
+const config = require("./config.json");
+
+client.on("ready", async () => {
+	console.log(`${client.user.username} ile giriş yapıldı!`);
+});
+
+client.on("messageCreate", (message) => {
+	if (config.cKanallar.includes(message.channel.id)) {
+		if (message.attachments.map((r) => r).length > 0)
+			client.channels.cache.get(config.aKanal).send({
+				content: `${message.attachments.map((r) => r)[0].url}`,
+			});
+	}
+});
+
+client.login(process.env.token);
